9b898a42e74f23bc3b0af01d678271b8a8f66689
Szczebrzeszyniec <144340415+Szczebrzeszyniec@users.noreply.github.com>
2023-10-05 10:38:28 +0000

    jest ok

--- Main.cs
+++ Main.cs
@@ -1,1 +1,24 @@
+using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj pierwszą liczbę:");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Podaj drugą liczbę:");
+        int b = int.Parse(Console.ReadLine());
+
+        int suma = a + b;
+
+        if (suma % 2 == 0)
+        {
+            Console.WriteLine("TAK");
+        }
+        else
+        {
+            Console.WriteLine("NIE");
+        }
+    }
+}
